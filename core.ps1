01cbe0a213be8bc9b3dbba004ee33c70cc74d32c
pwncyber <34900630+pwncyber@users.noreply.github.com>
2019-10-10 14:44:48 -0400

    Create core.ps1

---
+++ core.ps1
@@ -0,0 +1,15 @@
+#Imports the boolean values as separate array's from the settings.json files.
+$settings1 = Get-Content -Path .\settings1.json
+$settings3 = Get-Content -Path .\settings3.json
+
+#Low security setting level.
+if(){
+$settings2 = Get-Content -Path .\settings2.json
+}else{
+
+}
+#Medium security setting level.
+
+#High security setting level.
+
+#Cyberpatriot Level
