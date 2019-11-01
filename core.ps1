19720d1a1f26daf771579abd9c8d0431f305b0cc
pwncyber <34900630+pwncyber@users.noreply.github.com>
2019-11-01 12:48:12 -0400

    Update core.ps1

--- core.ps1
+++ core.ps1
@@ -1,27 +1,46 @@
-#Imports the boolean values as separate array's from the settings.json files.
+##############################################################################
+#Imports the boolean values as separate array's from the settings.json files.#
+##############################################################################
 $Networking = Get-Content -Path .\Networking.json
 $LocalSecPol = Get-Content -Path .\LocalSecPol.json
 $Lusrmgr = Get-Content -Path .\Lusrmgr.json
 $Services = Get-Content -Path .\Services.json
 $SecPol = Get-Content -Path .\SecPol.json
+$CyPat = Get-Content -Path .\CyPat.json
+$Password = Get-Content -Path .\Password.json
 
-
-#General Networking in Windows.
-
+################################
+#General Networking in Windows.#
+################################
 if($Networking[0]){
 #Refreshes DNS of the system.
 }
 if($Networking[1]){
 }
-#Local Security Policy
+
+#######################
+#Local Security Policy#
+#######################
 if($LocalSecPol[0]){
-#Refreshes DNS of the system.
 }
-#Local User Manager
+
+####################
+#Local User Manager#
+####################
 if($Lusrmgr[0]){
-#Refreshes DNS of the system.
 }
-#Windows Services
+
+##################
+#Windows Services#
+##################
 if($Services[0]){
-#Refreshes DNS of the system.
+}
+
+##############
+#Cyberpatriot#
+##############
+#Changes all user passwords to the one stored in the Password.json file.
+if($CyPat[0]){
+Get-LocalUser |
+    Set-Localuser -Password $Password
 }
