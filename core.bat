a868dc25700ca8230e0fd59828afb4521a3d449c
pwncyber <34900630+pwncyber@users.noreply.github.com>
2019-11-24 13:32:40 -0500

    Update core.bat

--- core.bat
+++ core.bat
@@ -1,3 +1,28 @@
-REM Set file variables.
-set /p Networking=<Networking.json
-echo Networking
+@echo off
+
+color 0b
+
+title Vector Shield
+REM Below is the file import section for batch files.
+set "CyPatRead=Cypat.json"
+If Not Exist "%CyPatRead%" (Goto :Error)
+rem This will read a file into an array of variables and populate it 
+setlocal EnableExtensions EnableDelayedExpansion
+for /f "delims=" %%a in ('Type "%CyPatRead%"') do (
+    set /a count+=1
+    set "Line[!count!]=%%a"
+)
+rem Display array elements
+For /L %%i in (1,1,%Count%) do (
+    echo "CyPat%%i" is assigned to ==^> "!Line[%%i]!"
+)
+pause>nul
+Exit
+::***************************************************
+:Error
+cls & Color c
+echo(
+echo   The file "%CyPatRead%" does not exist, make sure that you have not modified or changed the .json in any way.
+Pause>nul
+exit /b
+::***************************************************
