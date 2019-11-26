b10b1186edab21527011df761dd85ee6004ec6a9
pwncyber <34900630+pwncyber@users.noreply.github.com>
2019-11-25 22:28:13 -0500

    Update core.bat

--- core.bat
+++ core.bat
@@ -2,13 +2,45 @@
 
 color 0b
 
+echo[
+
+type ASCII.txt
+
 title Vector Shield
 
+echo[
 
-REM ==================================Imports CyPat.json============================================
+REM ==================================Imports Networking.json============================================
+set "File2Read=Networking.json"
+rem This will read a file into an array of variables and populate it 
+setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
+for /f "delims=" %%a in ('Type "%File2Read%"') do (
+    set /a count+=1
+    set "Networking[!count!]=%%a"
+)
+rem Display array elements
+For /L %%i in (1,1,%Count%) do (
+    echo "Networking%%i" is assigned to ==^> "!Networking[%%i]!"
+)
+REM ==================================Imports LocalSecPol.json============================================
+set "File2Read=LocalSecPol.json"
+rem This will read a file into an array of variables and populate it 
+setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
+for /f "delims=" %%a in ('Type "%File2Read%"') do (
+    set /a count+=1
+    set "LocalSecPol[!count!]=%%a"
+)
+rem Display array elements
+For /L %%i in (1,1,%Count%) do (
+    echo "LocalSecPol%%i" is assigned to ==^> "!LocalSecPol[%%i]!"
+)
+REM ==================================Imports Lurmgr.json============================================
 set "File2Read=Lusrmgr.json"
 rem This will read a file into an array of variables and populate it 
 setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
 for /f "delims=" %%a in ('Type "%File2Read%"') do (
     set /a count+=1
     set "Lusrmgr[!count!]=%%a"
@@ -17,17 +49,40 @@ rem Display array elements
 For /L %%i in (1,1,%Count%) do (
     echo "Lusrmgr%%i" is assigned to ==^> "!Lusrmgr[%%i]!"
 )
-REM ==================================Imports LocalSecPol.json============================================
-set "File2Read=LocalSecPol.json"
+REM ==================================Imports Services.json============================================
+set "File2Read=Services.json"
 rem This will read a file into an array of variables and populate it 
 setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
 for /f "delims=" %%a in ('Type "%File2Read%"') do (
     set /a count+=1
-    set "LocalSecPol[!count!]=%%a"
-	echo %Count%
+    set "Services[!count!]=%%a"
 )
 rem Display array elements
-For /L %%i in (1,1,3) do (
-    echo "LocalSecPol%%i" is assigned to ==^> "!LocalSecPol[%%i]!"
+For /L %%i in (1,1,%Count%) do (
+    echo "Services%%i" is assigned to ==^> "!Services[%%i]!"
 )
+REM ==================================Imports CyPat.json============================================
+set "File2Read=CyPat.json"
+rem This will read a file into an array of variables and populate it 
+setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
+for /f "delims=" %%a in ('Type "%File2Read%"') do (
+    set /a count+=1
+    set "CyPat[!count!]=%%a"
+)
+rem Display array elements
+For /L %%i in (1,1,%Count%) do (
+    echo "CyPat%%i" is assigned to ==^> "!CyPat[%%i]!"
+)
+REM ==================================Imports Password.json============================================
+set "File2Read=Password.json"
+rem This will read a file into an array of variables and populate it 
+setlocal EnableExtensions EnableDelayedExpansion
+    set /a count = 0
+set /p Password=<Password.json
+rem Display array elements
+    echo "Password" is assigned to ==^> "!Password!"
+
+
 pause
