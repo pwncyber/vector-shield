1744376d5602214be96befc74003780ba23ea771
Guy <49598099+Xioner121@users.noreply.github.com>
2019-11-30 18:31:33 -0500

    Add files via upload

--- Gui.java
+++ Gui.java
@@ -901,7 +901,7 @@ public class Gui extends Application {
 CheckBox[] AllBoxes = {Netroot, NetSecurity, NetProtocol, SecpolRoot, SecpolSecurity, SecpolMedia, SecpolClient, SecpolUser, LusrmgrRoot, LusrmgrAccount, LusrmgrAccess, LusrmgrAction, LusrmgrLogon, LusrmgrMaintenance, LusrmgrBasic, ServiceRoot, ServiceNetwork, ServiceShare, ServiceAccess, ServiceNetpro, ServiceWindows, ServiceGeneric, N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17, S18, S19, S20, S21, S22, L0, L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, L14, L15, L16, L17, L18, L19, L20, L21, L22, L23, L24, L25, L26, L27, L28, R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, cyPat0, cyPat1, cyPat2, cyPat3, cyPat4};
                //Low medium and high buttons
                    low.setOnAction(e -> {
-            CheckBox[] LowBoxes = {Netroot, NetSecurity, NetProtocol, LusrmgrLogon, LusrmgrAccount, N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, S0, S1, S3, S4, S5, S6, S10, S11, S12, S13, S17, S18, S21, L0, L1, L3, L4, L11, L12, L13, L15, L17, L20, R5, R6, R8, R15, R17, R22, R23, R33};
+            CheckBox[] LowBoxes = {NetSecurity, LusrmgrLogon, LusrmgrAccount, N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, S0, S1, S3, S4, S5, S6, S10, S11, S12, S13, S17, S18, S21, L0, L1, L3, L4, L11, L12, L13, L15, L17, L20, R5, R6, R8, R15, R17, R22, R23, R33};
             CheckBox[] NoBoxes = {};
        if (low.isSelected()) {
          changeBoxes(LowBoxes, AllBoxes);
