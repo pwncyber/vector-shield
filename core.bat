21b256e031d5aa0d0eb2c5bc55e267101a47b419
Guy <49598099+Xioner121@users.noreply.github.com>
2019-12-01 16:55:26 -0500

    Update core.bat

--- core.bat
+++ core.bat
@@ -160,7 +160,7 @@ if %LocalSecPol[7]%==true (
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa /v LmCompatibilityLevel /t REG_DWORD /d 1 /f
 )
 if %LocalSecPol[8]%==true (
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v CachedLogonsCount /t REG_DWORD /d 0 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v CachedLogonsCount /t REG_SZ /d 0 /f
 )
 if %LocalSecPol[14]%==true (
 echo Limit Local Use of Blank Passwords to Console Only
