532b82f5c87b00b4040dff2984db9677aa379220
Guy <49598099+Xioner121@users.noreply.github.com>
2019-12-05 23:03:48 -0500

    Update core.bat

--- core.bat
+++ core.bat
@@ -651,13 +651,8 @@ sc stop XboxNetApiSvc
 sc config XboxNetApiSvc start= disabled
 )
 if %Services[34]%==true (
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v AutomaticMaintenanceEnabled /t REG_DWORD /d 0 /f
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallDay /t REG_DWORD /d 0 /f
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallTime /t REG_DWORD /d 3 /f
-REG ADD HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallEveryWeek /t REG_DWORD /d 1 /f
-gpupdate /force
+sc config wuauserv start= auto
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
 )
 if %Services[35]%==true (
 sc stop Spooler
