953555728e444aac6a6ea14e096742bd95a52798
Guy <49598099+Xioner121@users.noreply.github.com>
2020-01-25 18:47:37 -0500

    Update core.bat

--- core.bat
+++ core.bat
@@ -613,6 +613,7 @@ sc stop TermService
 sc config TermService start= disabled
 sc stop UmRdpService
 sc config UmRdpService start= disabled
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
 )
 if %Services[17]%==true (
 sc stop RpcLocator
